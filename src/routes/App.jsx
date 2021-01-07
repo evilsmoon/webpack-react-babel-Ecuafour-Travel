56fe1cda7b1fc5ba3ee9f0bb364f54e1d63f17ca
Gabriel Morales <gabrielmorales201564@hotmail.com>
2021-01-06 20:00:49 -0500

    initial commit and config

---
+++ src/routes/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+// import { BrowserRouter, Switch, Route } from 'react-router-dom';
+export const App = () => {
+    return (
+        <div>
+            <h1>hellow</h1>
+        </div>
+    )
+};
